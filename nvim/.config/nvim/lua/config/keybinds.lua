fc88dc26ab59742bbac82b47b1262c6ed1f6247e
Muhammad Ridwan Farizqi <mridwan6661@gmail.com>
2026-07-28 15:48:31 +0700

    (nvim): keybind for q, w, x

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>q", vim.cmd.q)
+vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>x", vim.cmd.x)
