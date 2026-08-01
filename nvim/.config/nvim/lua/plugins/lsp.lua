93aedecd5aae9f1f5c2d5253203040df03952ed1
Muhammad Ridwan Farizqi <mridwan6661@gmail.com>
2026-08-01 20:25:05 +0700

    (nvim) add mason

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,24 @@
 return {
     {
-    "neovim/nvim-lspconfig",
-    config = function()
-        vim.lsp.config("clangd", {})
-        vim.lsp.enable("clangd")
-    end,
+        "mason-org/mason-lspconfig.nvim",
+        opts = {},
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            { 
+            "neovim/nvim-lspconfig",
+            config = function()
+                vim.lsp.config("clangd", {})
+                vim.lsp.enable("clangd")
+
+                vim.lsp.config("lua_ls", {
+                    settings = {
+                        Lua = {
+                            diagnostics = { globals = { "vim" } }
+                        }
+                    }
+                })
+            end,
+            },
+        },
     },
 }
