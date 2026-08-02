19dc66650d6b920f7b024063ec4dea2bacbb063f
Muhammad Ridwan Farizqi <mridwan6661@gmail.com>
2026-08-02 16:59:34 +0700

    (nvim): set indent to 2 for js, ts

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,3 +6,12 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "html", "javascript", "typescript", "javascriptreact", "typescriptreact", "css", "json" },
+    callback = function()
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.tabstop = 2
+        vim.opt_local.softtabstop = 2
+    end,
+})
