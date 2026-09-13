b80f9400bd0a19230b44866c12504f4ac1d1183c
Muhammad Ridwan Farizqi <mridwan6661@gmail.com>
2026-09-13 13:46:59 +0700

    (nvim): set event for indent line

--- nvim/.config/nvim/lua/plugins/generals.lua
+++ nvim/.config/nvim/lua/plugins/generals.lua
@@ -1,41 +1,43 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        lazy = false,
-        build = ":TSUpdate"
-    },
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        ---@module "ibl"
-        ---@type ibl.config
-        opts = {
-            scope = { enabled = true },
-            indent = {
-            char = "┊"
-            },
-        },
-    },
-    {
-	    "lewis6991/gitsigns.nvim"
-    },
-    {
-        "nvim-telescope/telescope.nvim", version = "*",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-        },
-        keys = {
-            { "<leader>ff", "<cmd>Telescope find_files<cr>" },
-            { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
-            { "<leader>fb", "<cmd>Telescope buffers<cr>" },
-        },
-    },
-    {
-        "windwp/nvim-autopairs",
-        event = "InsertEnter",
-        config = true
-        -- use opts = {} for passing setup options
-        -- this is equivalent to setup({}) function
-        },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate",
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			scope = { enabled = true },
+			indent = {
+				char = "┊",
+			},
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "*",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
+		keys = {
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>" },
+		},
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equivalent to setup({}) function
+	},
 }
