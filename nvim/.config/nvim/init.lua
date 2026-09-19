4a2c5ea8c1da5543a2f7f6eed85d1e8b6ad58c61
Muhammad Ridwan Farizqi <mridwan6661@gmail.com>
2026-09-19 12:52:37 +0700

    (nvim): add custom command to compile with gcc

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,17 +1,18 @@
 require("config.options")
 require("config.keybinds")
 require("config.lazy")
+require("config.cmds")
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "lua", "python", "javascript", "typescript", "c", "cpp" }, 
-    callback = function()
-	vim.treesitter.start()
-    end,
+	pattern = { "lua", "python", "javascript", "typescript", "c", "cpp" },
+	callback = function()
+		vim.treesitter.start()
+	end,
 })
 
 vim.diagnostic.config({
-  virtual_text = true, 
-  signs = false,        
-  underline = true,    
-  update_in_insert = false, 
+	virtual_text = true,
+	signs = false,
+	underline = true,
+	update_in_insert = false,
 })
---
+++ nvim/.config/nvim/lua/config/cmds.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_create_user_command("Gcc", function()
+	vim.cmd("!gcc % -o %:r")
+end, {})
