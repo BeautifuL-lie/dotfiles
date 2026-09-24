0884bceb758313b3ecb869c557eae3e063589eb2
Muhammad Ridwan Farizqi <mridwan6661@gmail.com>
2026-09-24 20:36:48 +0700

    (nvim): opt confirm = true

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -7,11 +7,13 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.confirm = true
+
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "html", "javascript", "typescript", "javascriptreact", "typescriptreact", "css", "json" },
-    callback = function()
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.tabstop = 2
-        vim.opt_local.softtabstop = 2
-    end,
+	pattern = { "html", "javascript", "typescript", "javascriptreact", "typescriptreact", "css", "json" },
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+	end,
 })
